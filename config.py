bdc5c88dbdad86d7786390b3103360687848fbf4
K Rin <rin@sandb0x.tw>
2024-10-23 16:19:28 +0000

    Add initial commit

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    DEBUG = False
+    ALLOW_CORS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    ALLOW_CORS = True
+
+def get_runtime_config():
+    return os.getenv("RUNTIME_CONFIG", "Development")
